d27ea9d77a2cbe640d0aca2edba2847b4498d275
Geoffrey Xiang <geoffreyxiang@berkeley.edu>
2025-11-24 21:06:34 -0800

    basic neotree -- potentially can be updated

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+--- FOLLOW https://github.com/hendrikmi/dotfiles/blob/main/nvim/lua/plugins/neo-tree.lua for more
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal" },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+}
